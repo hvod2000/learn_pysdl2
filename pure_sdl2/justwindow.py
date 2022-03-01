95ec2217db3442ce0dbd35fb6c2305f7a2d9e8d9
Uladzislau Khamkou <kham@tuta.io>
2022-03-01 19:43:01 +0300

    create empty window using pure sdl2

---
+++ pure_sdl2/justwindow.py
@@ -0,0 +1,18 @@
+from sdl2 import *
+
+window = SDL_CreateWindow(
+    "Very original window title with unicode: αβγ".encode(),
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    800,
+    450,
+    SDL_WINDOW_SHOWN,
+)
+
+done = False
+while not done:
+    e = SDL_Event()
+    while SDL_PollEvent(e):
+        done |= e.type == SDL_QUIT
+SDL_DestroyWindow(window)
+SDL_Quit()
